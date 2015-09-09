9d7fa09bc39bc854e2ec37ac0a1ddedba8c1a04c
caffgeek <gibble@gmail.com>
2015-09-08 21:46:27 -0500

    reorg

---
+++ test/linkcollector.js
@@ -0,0 +1,16 @@
+var expect = require("chai").expect;
+var request = require("request");
+
+describe('Link Collector API', function() {
+  describe('POST link to Collector', function () {
+   
+    var url = "http://localhost:1337";
+
+    it("returns status 200", function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+});
